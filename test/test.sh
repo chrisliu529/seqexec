c6c2bc87a308cd9e25ff45ce26d3964803332b77
Chris Liu <liugh@rippletek.com>
2017-09-14 14:03:17 +0800

    test with different keys

--- test/test.sh
+++ test/test.sh
@@ -6,12 +6,28 @@ step=1000000
 for (( i=0; i<$total; i+=$step )); do
     ./seqexec --key=test ./inc $shmid $step &
 done
+
+shmid2=`./alloc`
+for (( i=0; i<$total; i+=$step )); do
+    ./seqexec --key=test2 ./inc $shmid2 $step &
+done
+
 sleep 10
 n=`./read $shmid`
 ipcrm -m $shmid
 if [ "$total" == "$n" ]; then
-    echo "test ok"
+    echo "test1 ok"
+else
+    echo "test nok: $n"
+    exit 1
+fi
+
+n=`./read $shmid2`
+ipcrm -m $shmid2
+if [ "$total" == "$n" ]; then
+    echo "test2 ok"
     exit 0
 fi
+
 echo "test nok: $n"
 exit 1
